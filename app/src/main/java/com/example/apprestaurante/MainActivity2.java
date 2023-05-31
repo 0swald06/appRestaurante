78bc66f9222c34bca983cf5673f620fa4618b0af
Oswaldo <oswaldoalvarado@gmail.com>
2023-05-31 16:46:13 -0400

    Se agrego el boton al mapa y un boton de regreso

--- app/src/main/java/com/example/apprestaurante/MainActivity2.java
+++ app/src/main/java/com/example/apprestaurante/MainActivity2.java
@@ -1,14 +1,41 @@
 package com.example.apprestaurante;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ActionMenuView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.apprestaurante.databinding.ActivityMain2Binding;
 
 public class MainActivity2 extends AppCompatActivity {
 
+private ActivityMain2Binding mBinding;
+
+    private ActionMenuView mAction;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        mBinding=ActivityMain2Binding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        mBinding.btnMap.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (MainActivity2.this, MainActivity3.class);
+                startActivity(intent);
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
